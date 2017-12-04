2b8ad140f57b8c4d0868d9712ee79977f3a5f495
Erik Ziegler <erik.sweed@gmail.com>
2017-12-04 10:56:18 +0100

    First pass at a landing page

--- source/_posts/hello-world.md
+++
@@ -1,4 +0,0 @@
----
-title: Cornerstone
----
-Cornerstone is an open source project with a goal to deliver a complete web based medical imaging platform.
---
+++ themes/cornerstone/source/js/common.js
@@ -0,0 +1,161 @@
+(function () {
+  initMobileMenu()
+  initCornerstoneElement()
+
+  if (PAGE_TYPE) {
+    initLocationHashFuzzyMatching()
+  }
+
+  function parseRawHash (hash) {
+    // Remove leading hash
+    if (hash.charAt(0) === '#') {
+      hash = hash.substr(1)
+    }
+
+    // Escape characters
+    try {
+      hash = decodeURIComponent(hash)
+    } catch (e) {}
+    return CSS.escape(hash)
+  }
+
+  function initLocationHashFuzzyMatching () {
+    var rawHash = window.location.hash
+    if (!rawHash) return
+    var hash = parseRawHash(rawHash)
+    var hashTarget = document.getElementById(hash)
+    if (!hashTarget) {
+      var normalizedHash = normalizeHash(hash)
+      var possibleHashes = [].slice.call(document.querySelectorAll('[id]'))
+        .map(function (el) { return el.id })
+      possibleHashes.sort(function (hashA, hashB) {
+        var distanceA = levenshteinDistance(normalizedHash, normalizeHash(hashA))
+        var distanceB = levenshteinDistance(normalizedHash, normalizeHash(hashB))
+        if (distanceA < distanceB) return -1
+        if (distanceA > distanceB) return 1
+        return 0
+      })
+      window.location.hash = '#' + possibleHashes[0]
+    }
+
+    function normalizeHash (rawHash) {
+      return rawHash
+        .toLowerCase()
+        .replace(/\-(?:deprecated|removed|replaced|changed|obsolete)$/, '')
+    }
+
+    function levenshteinDistance (a, b) {
+      var m = []
+      if (!(a && b)) return (b || a).length
+      for (var i = 0; i <= b.length; m[i] = [i++]) {}
+      for (var j = 0; j <= a.length; m[0][j] = j++) {}
+      for (var i = 1; i <= b.length; i++) {
+        for (var j = 1; j <= a.length; j++) {
+          m[i][j] = b.charAt(i - 1) === a.charAt(j - 1)
+            ? m[i - 1][j - 1]
+            : m[i][j] = Math.min(
+              m[i - 1][j - 1] + 1,
+              Math.min(m[i][j - 1] + 1, m[i - 1][j] + 1))
+        }
+      }
+      return m[b.length][a.length]
+    }
+  }
+
+  /**
+   * Mobile burger menu button and gesture for toggling sidebar
+   */
+
+  function initMobileMenu () {
+    var mobileBar = document.getElementById('mobile-bar')
+    var sidebar = document.querySelector('.sidebar')
+    var menuButton = mobileBar.querySelector('.menu-button')
+
+    menuButton.addEventListener('click', function () {
+      sidebar.classList.toggle('open')
+    })
+
+    document.body.addEventListener('click', function (e) {
+      if (e.target !== menuButton && !sidebar.contains(e.target)) {
+        sidebar.classList.remove('open')
+      }
+    })
+
+    // Toggle sidebar on swipe
+    var start = {}, end = {}
+
+    document.body.addEventListener('touchstart', function (e) {
+      start.x = e.changedTouches[0].clientX
+      start.y = e.changedTouches[0].clientY
+    })
+
+    document.body.addEventListener('touchend', function (e) {
+      end.y = e.changedTouches[0].clientY
+      end.x = e.changedTouches[0].clientX
+
+      var xDiff = end.x - start.x
+      var yDiff = end.y - start.y
+
+      if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0 && start.x <= 80) sidebar.classList.add('open')
+        else sidebar.classList.remove('open')
+      }
+    })
+  }
+
+  function initCornerstoneElement() {
+    var element = document.getElementById('demo-element');
+    var imageId = 'ctexample://1';
+    cornerstone.enable(element);
+
+    cornerstone.loadAndCacheImage(imageId).then(function(image) {
+      cornerstone.displayImage(element, image);
+
+      cornerstoneTools.mouseInput.enable(element);
+      cornerstoneTools.mouseWheelInput.enable(element);
+      // Enable all tools we want to use with this element
+
+      // ww/wc is the default tool for left mouse button
+      cornerstoneTools.wwwc.activate(element, 1);
+
+      // pan is the default tool for middle mouse button
+      cornerstoneTools.pan.activate(element, 2);
+
+      // zoom is the default tool for right mouse button
+      cornerstoneTools.zoom.activate(element, 4);
+
+      // zoom is the default tool for middle mouse wheel
+      cornerstoneTools.zoomWheel.activate(element);
+    });
+  }
+
+  function handleFullscreenClick(e) {
+    var container = e.target.parentNode;
+
+    if (!document.fullscreenElement && !document.mozFullScreenElement &&
+      !document.webkitFullscreenElement && !document.msFullscreenElement) {
+      if (container.requestFullscreen) {
+        container.requestFullscreen();
+      } else if (container.msRequestFullscreen) {
+        container.msRequestFullscreen();
+      } else if (container.mozRequestFullScreen) {
+        container.mozRequestFullScreen();
+      } else if (container.webkitRequestFullscreen) {
+        container.webkitRequestFullscreen();
+      }
+    }
+
+    var element = document.getElementById('demo-element');
+    cornerstone.resize(element, true);
+  }
+
+  var fullscreenButton = document.getElementById('fullscreen-button');
+  fullscreenButton.addEventListener('click', handleFullscreenClick);
+
+  function resizeHandler() {
+    var element = document.getElementById('demo-element');
+    cornerstone.resize(element, true);
+  }
+
+  window.addEventListener('resize', resizeHandler)
+})()
